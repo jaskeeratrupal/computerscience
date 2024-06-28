ac5b2926e37fd6382a785c8d6508d2bb9f0947a0
Jaskeerat <jrupal@algomau.ca>
2024-06-28 01:52:22 -0400

    commit

---
+++ term_project/includes/footer.php
@@ -0,0 +1,3 @@
+<p style="text-align:center; background-color:black; color:white;">&#169;2024 Your Company Name.All rights reserved</p>
+</body>
+</html>
---
+++ term_project/index.php
@@ -0,0 +1,46 @@
+<?php require_once '../termproject/includes/header.php' ?>
+ "<!-- Animal list -->
+    <div class="container">
+      <!--row 1-->
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card mb-3">
+            <div class="d-flex gap-3">
+              <img
+                src="pics/penguins.jpg"
+                alt=""
+                class="img-fluid rounded-circle picture-size"
+              />
+            </div>
+          </div>
+        </div>
+        <!---->
+        <div class="col-md-4">
+          <div class="card mb-3">
+            <div class="d-flex gap-3">
+              <img
+                src="./pics/puppies.webp"
+                alt=""
+                class="img-fluid rounded-circle picture-size"
+              />
+            </div>
+          </div>
+        </div>
+        <!---->
+        <div class="col-md-4">
+          <div class="card mb-3">
+            <div class="d-flex gap-3">
+              <img
+                src="pics/cat.jpg"
+                alt=""
+                class="img-fluid rounded-circle picture-size"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+ <?php require_once '../termproject/includes/footer.php' ?>
+  
